0bbd65e73515e9d1a801334136681cbf5fec6fca
KarpenkoAnjey <karpenko.anjey@gmail.com>
2017-07-21 10:46:30 +0300

    a1

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -3,8 +3,6 @@
 # set environment variables used in deploy.sh and AWS task-definition.json:
 export IMAGE_NAME=netcoreapps-test
 export IMAGE_VERSION=latest
-
-
 export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST = ec2-13-58-60-220.us-east-2.compute.amazonaws.com
