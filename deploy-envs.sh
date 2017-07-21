19d5b8a63ff3dbc90830caa105df3fb636fb7f73
KarpenkoAnjey <karpenko.anjey@gmail.com>
2017-07-21 10:44:18 +0300

    pre...

---
+++ deploy-envs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=netcoreapps-test
+export IMAGE_VERSION=latest
+
+
+export AWS_DEFAULT_REGION=us-east-2
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST = ec2-13-58-60-220.us-east-2.compute.amazonaws.com
+
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# optional: SERVICESTACK_LICENSE
